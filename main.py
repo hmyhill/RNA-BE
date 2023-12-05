a637d65423001aa818d5fb22d681112fddeda291
hmyhill <henry.myhill201@gmail.com>
2023-12-05 14:09:16 +0000

    Adjusted API

--- main.py
+++ main.py
@@ -1,75 +1,60 @@
-from flask import app, request, Flask, jsonify
-from flask_wtf.csrf import CSRFProtect
+from flask import request, Flask, jsonify
 from newsdataapi import NewsDataApiClient
 
 # Initialises the API key
 api = NewsDataApiClient(apikey='pub_317102f7a1f35b5bb1aad1bc348030fc41397')
 
 app = Flask(__name__)
-csrf = CSRFProtect(app)
-csrf.init_app(app)
 
 
 @app.route('/news/top', methods=['GET'])
 def top_news():
     response = api.news_api(country='gb', language='en', domain='bbc', full_content=True, max_result=13)
-    print(response)
+    return response
 
 
 @app.route('/news/tech', methods=['GET'])
 def tech_news():
     response = api.news_api(country='gb', language='en', domain='bbc', full_content=True, max_result=13,
                             category='technology')
-    print(response)
+    return response
 
 
 @app.route('/news/sport', methods=['GET'])
 def sport_news():
     response = api.news_api(country='gb', language='en', domain='bbc', full_content=True, max_result=13,
                             category='sports')
-    print(response)
+    return response
 
 
 @app.route('/news/entertainment', methods=['GET'])
 def entertainment_news():
     response = api.news_api(country='gb', language='en', domain='bbc', full_content=True, max_result=13,
                             category='entertainment')
-    print(response)
+    return response
 
 
 @app.route('/news/world', methods=['GET'])
 def world_news():
     response = api.news_api(country='gb', language='en', domain='bbc', full_content=True, max_result=13,
                             category='world')
-    print(response)
+    return response
 
 
 @app.route('/user/create', methods=['POST'])
 def user_create():
-    if request.method == 'POST':
-        name = request.form['name']
-        role = request.form['role']
-        email = request.form['email']
-        password = request.form['password']
-        response = jsonify({'User Created'})
-        response.status_code = 200
-    else:
-        response = jsonify({'Invalid Request'})
-        response.status_code = 400
-        return response
+    name = request.json['name']
+    role = request.json['role']
+    email = request.json['email']
+    password = request.json['password']
+    return request.json
 
 
 @app.route('/user/login', methods=['GET'])
 def user_login():
-    if request.method == 'GET':
-        email = request.form['email']
-        password = request.form['password']
-        response = jsonify({'User Logged In'})
-        response.status_code = 200
-    else:
-        response = jsonify({'Invalid Request'})
-        response.status_code = 400
-        return response
+    email = request.json['email']
+    password = request.json['password']
+    return request.json
 
 
 @app.route('/user', methods=['GET'])
