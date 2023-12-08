80ccce822be4ac1cd328ab36176ea067fea8bf84
CaitlinHEvans <148791681+CaitlinHEvans@users.noreply.github.com>
2023-12-08 18:37:19 +0000

    Update views.py

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -3,6 +3,29 @@ from django.shortcuts import render
 from .models import Post
 from django.views import generic
 from django.shortcuts import render, redirect
+import json
+
+TAGS = (
+    (0, "---"),
+    (1, "Entertainment"),
+    (2, "Sport"),
+    (3, "Technology"),
+    (4, "World")
+    )
+
+def return_news_articles(request):
+    print(Post.objects)
+    if request.method == "GET":
+        try:
+            arr = []
+            for x in Post.objects.values_list():
+                print(x)
+                arr.append({ 'title': x[1], 'content': x[2], 'tags': TAGS[x[7]][1] })
+            return JsonResponse({"articles": arr})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
+    return JsonResponse({"error": "Invalid Request Method"}, status=405)
 
 #User views the articles filtered by published articles sorted by created articles
 class PostList(generic.ListView):
